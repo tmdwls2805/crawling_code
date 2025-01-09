24d63e2a1afa143c865c9e1b7214d06e5ab9f93f
tmdwls2805 <qkrtmdwls280@gmail.com>
2025-01-09 11:50:08 +0900

    feat: 3번째 form 추가

--- gov24/senior.py
+++ gov24/senior.py
@@ -6,9 +6,9 @@ from bs4 import BeautifulSoup
 
 
 def crawl_senior():
-    first_form = []
-    second_form = []
-    third_form = []
+    first_form = []  # type: list
+    second_form = [] # type: list
+    third_form = [] # type: list
     first_form_sheet = {
         '제목': None,
         '지원형태': None,
@@ -22,7 +22,7 @@ def crawl_senior():
         '소관기관': None,
         '최종수정일': None,
         'url': None,
-    }
+    } # type: dict
     second_form_sheet = {
         '제목': None,
         '신청방법': None,
@@ -32,7 +32,8 @@ def crawl_senior():
         '구비서류': None,
         '수수료': None,
         'url': None,
-    }
+        '담당기관': None,
+    } # type: dict
     third_form_sheet = {
         '제목': None,
         '신청방법': None,
@@ -42,7 +43,8 @@ def crawl_senior():
         '구비서류': None,
         '수수료': None,
         'url': None,
-    }
+        '담당기관': None,
+    } # type: dict
     # 대상 URL
     url = "https://www.gov.kr"  # 메인 페이지 URL로 시작
     base_path = "/search?srhQuery=노인&collectionCd=service&textAnalyQuery=&policyType=&webappType=&realQuery=노인&pageSize=10&publishOrg=&sfield=&sort=RANK&condSelTxt=&reSrchQuery=&sortSel=RANK&pageIndex="
@@ -61,11 +63,11 @@ def crawl_senior():
         # 크롤링할 데이터 추출
         results = soup.find('div', class_='result_cont_list')
 
-        if page_index > 3:
-            break
-        # if not results:
-        #     print('All crawling is done!')
+        # if page_index > 3:
         #     break
+        if not results:
+            print('All crawling is done!')
+            break
 
         li_tag = results.find_all('li', class_='result_li_box')
         for li in li_tag:
@@ -111,9 +113,7 @@ def crawl_senior():
             content_index += 1
         print(f"Crawling is done!: page {page_index}")
         page_index += 1
-
-    return(third_form)
-
+        # time.sleep(3)
 
 def decode_html(html_tag):
     raw_html = html_tag.decode_contents()
@@ -241,16 +241,81 @@ def process_form_2(second_form_sheet, connect_soup):
         info_title = service_info[i].find('p', class_='tit').text
         sfs[info_title] = post_processing_text(service_info[i].find('p', class_='txt').text)
 
+    h4_tags = wrap_col.find_all('h4')
+    organ_h4 = None
+    for h4 in h4_tags:
+        if "제도를 담당하는 기관" in h4.get_text(strip=True):
+            organ_h4 = h4
+            break
+    
+    if organ_h4:
+        sfs['담당기관'] = organ_h4.get_text(" ", strip=True).replace('제도를 담당하는 기관:  ', '').strip()
+
     return sfs
 
 def process_form_3(third_form_sheet, connect_soup):
     tfs = copy.deepcopy(third_form_sheet)
+    # 제목
     tfs['제목'] = post_processing_text(connect_soup.find('h2').text)
+    # 테이블 데이터
+    table = connect_soup.find('div', class_='tbl-list border info-table')
+    if table:
+        rows = table.find_all('tr')
+        for row in rows:    
+            th_tags = row.find_all('th')
+            td_tags = row.find_all('td')
+            for th, td in zip(th_tags, td_tags):
+                title = th.text.strip()
+                content = td.text.strip()
+                tfs[title] = content
+
+    h4_tags = connect_soup.find_all('h4')  # 모든 h4 태그 검색
+    # 제도를 담당하는 기관
+    organ_h4 = None
+    for h4 in h4_tags:
+        if "제도를 담당하는 기관" in h4.get_text(strip=True):
+            organ_h4 = h4
+            break
+
+    if organ_h4:
+        organ = organ_h4.find_next('ul')
+        strong_tag = organ.find('strong')
+        if strong_tag:
+            tfs['담당기관'] = strong_tag.get_text(" ", strip=True).strip()
+
+    # 구비서류
+    required_docs_h4 = None
+    for h4 in h4_tags:
+        if "신청 시 같이 제출 해야하는 서류(구비서류)" in h4.get_text(strip=True):
+            required_docs_h4 = h4
+            break
+
+    required_docs_result = []
+    if required_docs_h4:
+        required_docs_tags = required_docs_h4.find_next('ul').find_all('li')
+        for required_docs_tag in required_docs_tags:
+            if "민원인이 제출해야 하는 서류" in required_docs_tag.get_text(strip=True):
+                required_docs = required_docs_tag.find('ul', class_='sub-list').find_all('p')
+                for required_doc in required_docs:
+                    required_docs_result.append(required_doc.get_text(strip=True).replace('-', ''))
+    tfs['구비서류'] = required_docs_result
+
+    # 참고정보
+    ref_h4 = None
+    for h4 in h4_tags:
+        if "참고정보" in h4.get_text(strip=True):
+            ref_h4 = h4
+            break
+    if ref_h4:
+        ref = ref_h4.find_next('ul')
+        date_li = ref.find('ul', class_='sub-list').find_all('li')[-1]
+        
+        if date_li:
+            date = date_li.get_text(strip=True)
+            tfs['최종수정일'] = date
 
     return tfs
 
-print(crawl_senior())
-
+crawl_senior()
 
 # excel로 출력하는 기능 추가 (sheet별로)
-# form_3 테이블 정보 가져오기
